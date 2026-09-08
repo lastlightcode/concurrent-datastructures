b7ae733d673c153df1beeca3bbe90f37df1b4101
Dipen Lakhani <13121210+dips7189@users.noreply.github.com>
2026-09-08 23:19:21 +0100

    Dequeue implementation attempted

--- src/main/java/org/dips/datastructure/queue/BasketsQueue.java
+++ src/main/java/org/dips/datastructure/queue/BasketsQueue.java
@@ -5,6 +5,8 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public final class BasketsQueue<T> implements ConcurrentQueue<T> {
 
+  private static final int MAX_JUMPS = 16;
+
   private final AtomicReference<Node<T>> head;
   private final AtomicReference<Node<T>> tail;
   private final int MAX_RETRY_ATTEMPTS;
@@ -69,7 +71,73 @@ public final class BasketsQueue<T> implements ConcurrentQueue<T> {
   }
 
   public T dequeue() {
-    throw new UnsupportedOperationException("Not yet implemented ...");
+    dequeue:
+    while (true) { // dequeue loop
+      var obsrvdHead = head.get();
+      var obsrvdTail = tail.get();
+
+      if (obsrvdHead != head.get()) {
+        continue;
+      }
+
+      var next = obsrvdHead.next.getReference();
+
+      if (obsrvdHead == obsrvdTail) {
+        if (next == null) {
+          return null; // the queue is empty
+        }
+
+        // head & tail are the same but next points to something so we have a lagging tail
+        // catch up the lagging tail
+        var candidate = next;
+
+        while (candidate.next.getReference() != null && tail.get() == obsrvdTail) {
+          candidate = candidate.next.getReference();
+        }
+
+        tail.compareAndSet(obsrvdTail, candidate);
+      }
+
+      // otherwise:
+      // search from head for first unmarked link
+      int jumps = 0;
+      boolean[] markHolder = new boolean[1];
+      var current = obsrvdHead;
+
+      while (true) { // traverse chain of dead nodes
+        var candidate = current.next.get(markHolder);
+        var deleted = markHolder[0];
+
+        if (candidate == null) {
+          // reached physical end
+          // everything traversed so far was dead
+          if (current != obsrvdHead) {
+            head.compareAndSet(obsrvdHead, current);
+          }
+
+          return null;
+        }
+
+        if (!deleted) {
+          // candidate is the first live node
+          // try to mark current.next
+          if (current.next.compareAndSet(candidate, candidate, false, true)) {
+            // clean when jumps reaches MAX_JUMPS
+            if (jumps >= MAX_JUMPS) {
+              head.compareAndSet(obsrvdHead, candidate);
+            }
+
+            return candidate.value;
+          }
+
+          continue dequeue;
+        }
+
+        // candidate is logically deleted
+        jumps++;
+        current = candidate;
+      }
+    }
   }
 
   static final class Node<T> {
